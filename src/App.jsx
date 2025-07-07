55bb0cd7888447c27164e2d8157fce69a1511fc4
Priyanshu Bhardwaj <priyanshubhardwaj0888@gmail.com>
2025-07-07 23:10:04 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 
-const s = () => {
+const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" />
-        <Route path="/blog" element={<Blog/>} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/blog/:id" element={<Blog/>} />
       </Routes>
         <Home />
     </div>
   )
 }
 
-export default s
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     const navigate = useNavigate();
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
-       <img onClick={()=>navigate('/')} src= {assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer'/>
+       <img onClick={()=>navigate("/")} src= {assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer'/>
        <button onClick={()=>navigate('/admin')}  className='flex items-center gap-2 rounded-full text-sm
-       cursor-pointer bg-primary text -white px-10 py-2.5'>
+       cursor-pointer bg-primary text-white px-10 py-2.5'>
         login
         <img src={assets.arrow} alt="arrow" className='w-3' />
        </button>
@@ -15,4 +15,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
