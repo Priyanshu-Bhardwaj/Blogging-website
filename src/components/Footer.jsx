89469718c5836f9c29990d5d73b98445c7353b56
Priyanshu Bhardwaj <priyanshubhardwaj0888@gmail.com>
2025-07-16 23:55:33 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3'>
-        <div>
-
+        <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500'> 
+            <div>
+                 <img src={assets.logo} alt="logo" className='w-32 sm:w-44'/>
+                 <p className='max-w-[410px] mt-6'>Lorem, ipsum dolor sit amet 
+                   consectetur adipisicing elit. Cum facilis, corporis dolores reprehenderit 
+                  accusantium maiores nesciunt dignissimos maxime.</p>
+            </div>
         </div>
         <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>
         Copyright 2025 @QuickBlog. All rights reserved.</p>
